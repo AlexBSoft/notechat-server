a6ead88f123da48b788bd5d2ce15da66f159abd0
Alex <alexbs.ga@gmail.com>
2022-06-20 12:25:55 +0300

    Sessions

--- readme.md
+++ readme.md
@@ -1,3 +1,38 @@
 # Notechat server
 
-Server for notechat app make with Express and Socket.io
+Server for notechat app made with Express and Socket.io
+
+## Protocol
+
+Socket.io
+
+## Events
+
+События, которые используются в протоколе
+
+
+- [client] connect
+- [server] session
+- [server] users
+- [server] past_history
+- [server] user_connected
+- [client / server] message
+- [client] disconnect
+- [server] user_disconnected
+ 
+
+## Development
+
+```
+npm run dev
+```
+
+## Deployment
+
+Deploy on heroku or where you want
+
+## Notechat
+
+- Desktop client [https://github.com/zoxione/Notechat-desktop](https://github.com/AlexBSoft/notechat-server)
+
+- 
--- src/index.js
+++ src/index.js
@@ -24,7 +24,38 @@ app.get('/version/clientdesktop', (req, res) => {
     res.send(versions.clientdesktop);
 });
 
+const crypto = require("crypto");
+const randomId = () => crypto.randomBytes(8).toString("hex");
+
+const { InMemorySessionStore } = require("./sessionStore");
+const sessionStore = new InMemorySessionStore();
+
+io.use((socket, next) => {
+    const sessionID = socket.handshake.auth.sessionID;
+    if (sessionID) {
+      const session = sessionStore.findSession(sessionID);
+      if (session) {
+        //console.log("1️⃣ Existing session");
+        socket.sessionID = sessionID;
+        socket.userID = session.userID;
+        return next();
+      }
+    }
+    socket.sessionID = randomId();
+    socket.userID = randomId();
+    //console.log("0️⃣ New session");
+    next();
+});
+
+// TODO: save all data to DB
 var document = [];
+/*
+{
+    text: "",
+    lastChanged: timestamp,
+    lastChangedBy: user
+}
+*/
 
 io.on('connection', (socket) => {
 
@@ -32,10 +63,20 @@ io.on('connection', (socket) => {
 
     var room = socket.handshake.query.room || "doc";
 
+    // persist session
+    sessionStore.saveSession(socket.sessionID, {
+        userID: socket.userID,
+        connected: true,
+    });
+
+    // emit session details
+    socket.emit("session", {
+        sessionID: socket.sessionID,
+        userID: socket.userID,
+    });
+
     socket.join(room);
     const users = [];
-
-    //console.log(io.sockets.adapter.rooms['doc']);
     // Send the user other users
     for (let [id, socket] of io.of("/").sockets) {
         users.push({
@@ -43,10 +84,12 @@ io.on('connection', (socket) => {
           //username: socket.username,
         });
     }
-    
     socket.emit("users", users);
 
-    socket.emit("past_history", document[room]);
+    if(!document[room])
+        document[room] = {}
+
+    socket.emit("past_history", document[room].text);
 
     // notify existing users
     socket.broadcast.to(room).emit("user_connected", {
@@ -59,7 +102,8 @@ io.on('connection', (socket) => {
     socket.on('message', (msg) => {
         console.log("",socket.id," ✉️ ",room," ===> ",  msg)
         //io.emit('message', msg);
-        document[room]=msg;
+        document[room].text=msg;
+        document[room].lastChanged = Date.now();
         socket.broadcast.to(room).emit("message", msg);
     });
 
@@ -67,6 +111,11 @@ io.on('connection', (socket) => {
         console.log('user disconnected ',socket.id);
         socket.broadcast.to(room).emit("user_disconnected", {
             userID: socket.id,
+        });
+
+        sessionStore.saveSession(socket.sessionID, {
+            userID: socket.userID,
+            connected: false,
         });
     });
 
